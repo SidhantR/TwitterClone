e738b6cdb2e1a3949131684732e674464a8fd01e
SidhantR <sidhantrc02@gmail.com>
2024-09-09 00:36:04 +0530

    createTweet handled from backend

--- twitter-server/src/app/index.ts
+++ twitter-server/src/app/index.ts
@@ -4,6 +4,7 @@ import { expressMiddleware } from '@apollo/server/express4';
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import {User} from './user'
+import {Tweet} from './tweet'
 import { GraphqlContext } from '../interfaces';
 import JWTService from '../services/jwt';
 
@@ -15,13 +16,22 @@ export async function initServer() {
     const graphqlServer = new ApolloServer<GraphqlContext>({
         typeDefs: `#graphql
           ${User.types}
+          ${Tweet.types}
+
           type Query {
             ${User.queries}
           }
+          type Mutation {
+            ${Tweet.mutations}
+          }
+
         `,
         resolvers: {
           Query: {
             ...User.resolvers.queries
+          },
+          Mutation: {
+            ...Tweet.resolvers.mutations
           }
         }
     })
